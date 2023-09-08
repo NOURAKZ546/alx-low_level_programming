b003b324c2bff3831cb6fccce057085750e06c12
NOURAKZ546 <nourakouaz1@gmail.com>
2023-09-08 14:19:40 -0700

    o

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 /**
  * main - main block
- * Description: Get a random number and check its last digit, compare it with 5
+ * Description: prints all single digit numbers of base 10
+ * starting from 0, followed by a new line.
  * Return: 0
  */
 int main(void)
-{ 
+{
 	char c = '0';
 
 	while (c <= '9')
