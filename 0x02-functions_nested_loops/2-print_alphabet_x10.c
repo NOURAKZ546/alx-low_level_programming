876de35b932d8e9f7945cb1430c6214daf27ab6b
NOURAKZ546 <nourakouaz1@gmail.com>
2023-09-13 05:14:58 -0700

    rr

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*print_alphabet_x10 - prints alphabet letters 10 times
+*
+*/
+void print_alphabet_x10(void)
+{
+int line, letters;
+for (line = 0; line <= 9; line++)
+{
+for (letters = 'a'; letters <= 'z'; letters++)
+_putchar(letters);
+_putchar('\n');
+}
