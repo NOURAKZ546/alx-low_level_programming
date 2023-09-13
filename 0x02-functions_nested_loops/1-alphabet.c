f46f49291826da2275c2fccf9c9b3994017a3b1a
NOURAKZ546 <nourakouaz1@gmail.com>
2023-09-13 01:54:19 -0700

    y

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*print_alphabet - function that print alphabet
+*it prints in lowercase
+*Return: no return
+*/
+void print_alphabet(void);
+{
+int alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+_putchar(10)
+}
