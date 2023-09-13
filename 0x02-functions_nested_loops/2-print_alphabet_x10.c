5bbb5b30bf566c145d17343aa7d8ebe0dc160a3e
NOURAKZ546 <nourakouaz1@gmail.com>
2023-09-13 02:23:48 -0700

    yn

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,22 @@
-#include "main.h"
+/*
+ * File: 2-print_alphabet_x10.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "holberton.h"
+
 /**
-*print_alphabet_x10 - prints alphabet letters
-* ten times
-*desc: The function uses loop to operate
-*Return: no return
-*/
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+ */
 void print_alphabet_x10(void)
 {
-int counter = 0;
-int letter;
-while (counter++ <= 9)
-{
-for (letter = 'a'; letter <= 'z'; letter++)
-_putchar(letter);
+	int count = 0;
+	char letter;
 
-_putchar(10);
-}
+	while (count++ <= 9)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
 }
